05e7a3c03fba64d5b9ca0e4050086a1a23d99190
Evgeny <xonix75@mail.ru>
2019-06-09 21:39:06 +0300

    Form

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
+use Cocur\Slugify\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,6 +31,31 @@ class PostsController extends AbstractController
         return $this->render('posts/index.html.twig', ['posts' => $posts]);
     }
 
+    /**
+     * @Route("/posts/new", name="new_blog_post")
+     * @param Request $request
+     * @param Slugify $slugify
+     * @return Response
+     * @throws \Exception
+     */
+    public function addPost(Request $request, Slugify $slugify)
+    {
+        $post = new Post;
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSlug($slugify->slugify($post->getTitle()));
+            $post->setCreatedAt(new \DateTime);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('blog_posts');
+        }
+        return $this->render('posts/new.html.twig', ['form' => $form->createView()]);
+    }
 
     /**
      * @Route("/posts/{slug}", name="blog_show")
