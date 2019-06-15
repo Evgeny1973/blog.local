61e62aac1e58c40d1cf076a91094a28acccb71e4
Evgeny <xonix75@mail.ru>
2019-06-15 16:43:41 +0300

    CRUD

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -6,7 +6,9 @@ use App\Entity\Post;
 use App\Form\PostType;
 use App\Repository\PostRepository;
 use Cocur\Slugify\Slugify;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,26 +38,63 @@ class PostsController extends AbstractController
      * @param Request $request
      * @param Slugify $slugify
      * @return Response
+     * @return RedirectResponse
      * @throws \Exception
      */
-    public function addPost(Request $request, Slugify $slugify)
+    public function addPost(Request $request, Slugify $slugify, EntityManagerInterface $em)
     {
         $post = new Post;
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
-            echo 123;
+        if ($form->isSubmitted() && $form->isValid()) {
             $post->setSlug($slugify->slugify($post->getTitle()));
-            $post->setCreatedAt(new \DateTime());
+            $post->setCreatedAt(new \DateTime);
 
-            $em = $this->getDoctrine()->getManager();
             $em->persist($post);
             $em->flush();
 
             return $this->redirectToRoute('blog_posts');
         }
         return $this->render('posts/new.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/posts/{slug}/edit", name="blog_post_edit")
+     * @param Post $post
+     * @param Request $request
+     * @param Slugify $slugify
+     * @return Response
+     * @return RedirectResponse
+     */
+    public function edit(Post $post, Request $request, Slugify $slugify)
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form ->isValid()) {
+            $post->setSlug($slugify->slugify($post->getTitle()));
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            
+            return $this->redirectToRoute('blog_show', ['slug' => $post->getSlug()]);
+        }
+        return $this->render('posts/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/posts/{slug}/delete", name="blog_post_delete")
+     * @param Post $post
+     * @return RedirectResponse
+     */
+    public function delete(Post $post)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+        return $this->redirectToRoute('blog_posts');
     }
 
     /**
